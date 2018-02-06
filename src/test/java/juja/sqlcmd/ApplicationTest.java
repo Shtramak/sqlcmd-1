edb9567b96ca6aa502bc0e82d2592eb49e9a0525
shtramak <shtramak@gmail.com>
2018-02-06 13:42:37 +0200

    #100 make changes in setConnection() method

--- src/test/java/juja/sqlcmd/ApplicationTest.java
+++ src/test/java/juja/sqlcmd/ApplicationTest.java
@@ -21,9 +21,7 @@ public class ApplicationTest {
     private static final String DB_ADMIN_LOGIN = "postgres";
     private static final String DB_ADMIN_PASSWORD = "postgres";
     private static final String DB_USER_LOGIN = "sqlcmd";
-    private static final String DB_USER_PASSWORD = "sqlcmd";
-    private static final String TEST_DB_NAME = "sqlcmd_test";
-
+    private static final String TEST_DB_NAME = "sqlcmd";
 
     private static Connection connection;
 
@@ -37,11 +35,7 @@ public class ApplicationTest {
     public static void setConnection() throws SQLException {
         connection = DriverManager.getConnection(DB_CONNECTION_URL + ADMIN_DB_NAME, DB_ADMIN_LOGIN, DB_ADMIN_PASSWORD);
         executeSqlQuery("DROP DATABASE IF EXISTS " + TEST_DB_NAME);
-        executeSqlQuery("CREATE DATABASE " + TEST_DB_NAME + " OWNER =" + DB_USER_PASSWORD);
-        connection.close();
-        connection = DriverManager.getConnection(DB_CONNECTION_URL + TEST_DB_NAME, DB_ADMIN_LOGIN, DB_ADMIN_PASSWORD);
-        executeSqlQuery("DROP DATABASE IF EXISTS " + DB_USER_LOGIN);
-        executeSqlQuery("CREATE DATABASE " + DB_USER_LOGIN + " OWNER " + DB_USER_PASSWORD);
+        executeSqlQuery("CREATE DATABASE " + TEST_DB_NAME + " OWNER =" + DB_USER_LOGIN);
         connection.close();
     }
 
